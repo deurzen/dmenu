2d8f26af2285d41dcb86cbcc8b0d778446f6cc1e
deurzen <m.deurzen@tum.de>
2019-10-31 02:27:56 +0100

    updates colorscheme

--- config.h
+++ config.h
@@ -10,8 +10,8 @@ static const char *fonts[] = {
 static const char *prompt = NULL; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*                fg         bg       */
-	[SchemeNorm] = { "#F8EBE7", "#454745" },
-	[SchemeSel]  = { "#ED764D", "#454745" },
+	[SchemeNorm] = { "#F8EBE7", "#2D2D2D" },
+	[SchemeSel]  = { "#ED764D", "#2D2D2D" },
 	[SchemeOut]  = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
